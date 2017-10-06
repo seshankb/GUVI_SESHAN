60c205b7e7dd669be173f1218a18a60846fe82d2
Seshankb <seshankb@gmail.com>
2017-10-06 14:29:50 +0530

    Create Guvi1.java
    This program is to check the number if it is positive or negative or zero.

---
+++ Guvi1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Guvi1 {
+   public static void main(String[] args)
+   { 
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter a number: ");
+         int numericValue = input.nextInt();
+              if( numericValue == 0)
+                     {
+                     System.out.println("zero");
+                     }
+              else if (numericValue > 0)
+                     {
+                     System.out.println("positive"); 
+                     }
+              else
+                     {
+                     System.out.println("negative"); 
+                     }
+  }
+                  }
