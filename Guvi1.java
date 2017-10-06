5e8dfd2e395815d5f2ba72aa9f86379fc1502ed4
Seshankb <seshankb@gmail.com>
2017-10-06 14:45:54 +0530

    Update Guvi1.java

--- Guvi1.java
+++ Guvi1.java
@@ -4,7 +4,6 @@ public class Guvi1 {
    public static void main(String[] args)
    { 
       Scanner input = new Scanner(System.in);
-      System.out.print("Enter a number: ");
          int numericValue = input.nextInt();
               if( numericValue == 0)
                      {
